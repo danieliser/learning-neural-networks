5f8f518914ab3e9d2f6dced93cdffefe97ba9fe2
Daniel Iser <daniel@code-atlantic.com>
2020-11-02 04:04:29 -0500

    Correct file names

--- 1. Learning to program with Python 3/1. Basics/lesson13 - gameplay pt1.py
+++ 1. Learning to program with Python 3/1. Basics/lesson13 - gameplay pt1.py
@@ -21,6 +21,7 @@ def win(current_game):
         diags.append(game[row][col])
     if diags[0] != 0 and diags.count(diags[0]) == len(diags):
         print(f"Player {diags[0]} is the winner diaganal (/)!")
+        return True
 
     # Vertical
     for col in range(len(game)):
@@ -36,27 +37,24 @@ def win(current_game):
 
 def game_board(game_map, player: int = 0, row: int = 0, column: int = 0, just_display: bool = False):
     try:
-        if game_map[row][column] != 0:
-            print("This position is occupied! Choose another!")
-            return game_map, False
-        print("   0  1  2")
+        print("   a  b  c")
         if not just_display:
             game_map[row][column] = player
         for count, row in enumerate(game_map):
             print(count, row)
-        return game_map, True
+        return game_map
 
     except IndexError as e:
         print("Error: make sure you input row/column as 0 1 or 2", e)
-        return game_map, False
 
     except Exception as e:
         print("Something went very wrong!", e)
-        return game_map, False
 
 
 play = True
 players = [1, 2]
+
+
 while play:
     game = [[0, 0, 0],
             [0, 0, 0],
@@ -68,9 +66,8 @@ while play:
     while not game_won:
         current_player = next(player_cycle)
         print(f"Current Player: {current_player}")
-
-        played = False
-        while not played:
-            row_choice = int(input("What row do you want to play? (0, 1, 2): "))
-            column_choice = int(input("What column do you want to play? (0, 1, 2): "))
-            game, played = game_board(game, current_player, row_choice, column_choice)
+        row_choice = int(input("What row do you want to play? (0, 1, 2): "))
+        column_choice = int(input("What column do you want to play? (0, 1, 2): "))
+        game = game_board(game, current_player, row_choice, column_choice)
+        if win(game):
+            game_won = True
